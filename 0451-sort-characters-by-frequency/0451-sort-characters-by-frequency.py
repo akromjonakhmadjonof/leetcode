faa8a06fe9f3504e6ad19e7097fe6774dd8dcaf5
Akromjon Akhmadjonov <akromjonakhmadjonoff@gmail.com>
2025-09-09 11:19:28 -0400

    Time: 11 ms (50.48%), Space: 20.7 MB (17.43%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,8 +1,10 @@
 from collections import Counter
 class Solution:
     def frequencySort(self, s: str) -> str:
-        counter = Counter(s)
-        n = len(s)
+        counter = Counter(s) # Counts the frequencies of chars
+        n = len(s) # Length of s
+
+        # Bucketing
         buckets = ["" for _ in range(n + 1)]
 
         for char, count in counter.items():
