aa1b019eea3f78628b72d25a8bcdba32c8aa5279
Akromjon Akhmadjonov <akromjonakhmadjonoff@gmail.com>
2025-09-09 18:55:24 -0400

    Time: 107 ms (40.92%), Space: 23.5 MB (23.31%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -1,10 +1,11 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        results = {}
-        results[0] = -1
-        max_len = 0
-        total = 0
+        results = {} # Stores in which position what value we had 
+        results[0] = -1 # Initially before the earliest element we have total value of 0 which is at -1
+        max_len = 0 # Store maximum lenght of subarray
+        total = 0 # Stores all numbers +1 if num in nums is 1 else -1
         
+        # Iterates through the list
         for i in range(len(nums)):
             total += -1 if not nums[i] else 1
             
