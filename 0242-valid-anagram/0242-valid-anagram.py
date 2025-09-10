aba1f95ba0427f8c47efe755705d944b6ca7008e
Akromjon Akhmadjonov <akromjonakhmadjonoff@gmail.com>
2025-09-10 10:13:28 -0400

    Time: 5 ms (92.79%), Space: 18 MB (50.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # If the lenght of strings are not the same then it is not anagram
         if len(s) != len(t):
             return False
 
-        s_count = Counter(s)
-        t_count = Counter(t)
+        
+        s_count = Counter(s) # Stores frequcenies of each char in the string S
+        t_count = Counter(t) # Stores frequcenies of each char in the string T
 
+        # Iterate through the frequency list of the string S and compare to the string T
         for char, count in s_count.items():
             if t_count[char] != count:
                 return False
