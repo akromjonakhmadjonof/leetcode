12e688cdb723910ba8238407015fbcf4e0174ca7
Akromjon Akhmadjonov <akromjonakhmadjonoff@gmail.com>
2025-09-11 09:04:56 -0400

    Time: 3 ms (81.89%), Space: 21.1 MB (23.22%) - LeetHub

--- 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.py
@@ -3,17 +3,17 @@ class MovingAverage:
     def __init__(self, size: int):
         self.size = size # Stores the size of sliding window
         self.queue = collections.deque() # Stores the elements in queue
-        
+        self.window_sum = 0
 
     def next(self, val: int) -> float:
 
         if len(self.queue) ==  self.size:
-            self.queue.popleft() # Pop from left if the length of the queue exceeds the size
+            self.window_sum -= self.queue.popleft() # Pop from left if the length of the queue exceeds the size
         
         # Returning the average of sliding window 
         self.queue.append(val)
-
-        return sum(self.queue) / len(self.queue)
+        self.window_sum += val
+        return self.window_sum / len(self.queue)
 
 
 # Your MovingAverage object will be instantiated and called as such:
