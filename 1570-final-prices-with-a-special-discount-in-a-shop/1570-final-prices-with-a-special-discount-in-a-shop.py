7fbcf9306779d6e2ea2bc910b36c3c797b9f0f1c
Akromjon Akhmadjonov <akromjonakhmadjonoff@gmail.com>
2025-09-12 18:28:28 -0400

    Time: 0 ms (100%), Space: 17.8 MB (49.26%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
@@ -1,10 +1,14 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        answers = prices[:]
-        stack = [ ]
+        answers = prices[:] # We are gonna copy prices list as default returning answer
+        stack = []
+
         for i in range(len(prices)):
+            # We are gonna check if the last item in stack is greater then current element 
+            # If yes then we have to apply discount if not we have to go on
             while stack and prices[stack[-1]] >= prices[i]:
                 index = stack.pop()
                 answers[index] = prices[index] - prices[i]
             stack.append(i)
+        
         return answers
