bce1a86ded8a2679c012789a14917a5cbb213591
Akromjon Akhmadjonov <akromjonakhmadjonoff@gmail.com>
2025-09-14 20:45:44 -0400

    Time: 1 ms (48.12%), Space: 18 MB (36.81%) - LeetHub

--- 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.py
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.py
@@ -1,8 +1,11 @@
 class Solution:
     def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
         brokenLetters = set(brokenLetters) # Turn into set for O(1) search
-        count = 0 # Counts the number of words that can be fully typed
+        count = 0 # Counts the number of words that can't be fully typed
         for word in text.split(" "):
-            if all(char not in brokenLetters for char in list(set(word))):
-                count += 1
-        return count
+            for char in word:
+                if char in brokenLetters:
+                    count += 1
+                    break
+            
+        return len(text.split(" ")) - count
