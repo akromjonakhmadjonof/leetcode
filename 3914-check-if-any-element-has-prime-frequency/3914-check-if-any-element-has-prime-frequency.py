20463b6565f32d93052c44b0189dbe803524a374
Akromjon Akhmadjonov <akromjonakhmadjonoff@gmail.com>
2025-09-15 19:41:46 -0400

    Time: 0 ms (100%), Space: 17.9 MB (46.85%) - LeetHub

--- 3914-check-if-any-element-has-prime-frequency/3914-check-if-any-element-has-prime-frequency.py
+++ 3914-check-if-any-element-has-prime-frequency/3914-check-if-any-element-has-prime-frequency.py
@@ -1,10 +1,21 @@
 from collections import Counter
 class Solution:
     def checkPrimeFrequency(self, nums: List[int]) -> bool:
-        nums = Counter(nums) # Stores the frequency of each num in nums
-        # Instead of algo to find if num is prime it is more optimal to hardcode since the nums.lenght is less than or equal to 100
-        primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97} 
-        for num, count in nums.items():
-            if count in primes:
+        # Check if n is prime
+        def check(n):
+            if n == 1:
+                return False
+            for i in range(2, n//2 + 1):
+                if n % i == 0:
+                    return False
+            return True
+
+        freq = {}
+
+        for i in nums:
+            freq[i] = freq.get(i, 0) + 1
+        
+        for i in freq.values():
+            if check(i) == True:
                 return True
         return False
