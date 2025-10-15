b8a14ac41cba01797ec2bc89b54b9a71d39baff1
Akromjon Akhmadjonov <akromjonakhmadjonoff@gmail.com>
2025-10-15 15:08:10 -0400

    Time: 11 ms (29.7%), Space: 22.5 MB (5.34%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -3,10 +3,12 @@ class Solution:
         n = len(nums)
         freq = {}
         results = []
+        one_third_of_n = n / 3
+
         for num in nums:
             freq[num] = freq.get(num, 0) + 1
         
         for num, count in freq.items():
-            if count > n / 3:
+            if count > one_third_of_n:
                 results.append(num)
         return results
