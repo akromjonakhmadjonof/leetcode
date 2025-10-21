249b6acaffb383a85c84337aecbf12a06bc72b3b
Akromjon Akhmadjonov <akromjonakhmadjonoff@gmail.com>
2025-10-21 13:26:04 -0400

    Time: 4 ms (40.88%), Space: 17.8 MB (51.28%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        
+        left, right = 1, x
+        while left <= right:
+            mid = left + (right - left) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid > x:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return right
+        
