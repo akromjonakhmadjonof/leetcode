07c7e55392ae95de6a8d34f6717d11eea1711b68
Akromjon Akhmadjonov <akromjonakhmadjonoff@gmail.com>
2025-11-16 11:34:54 -0500

    Time: 1 ms (20.5%), Space: 17.6 MB (86.34%) - LeetHub

--- 2372-rearrange-characters-to-make-target-string/2372-rearrange-characters-to-make-target-string.py
+++ 2372-rearrange-characters-to-make-target-string/2372-rearrange-characters-to-make-target-string.py
@@ -1,12 +1,18 @@
 class Solution:
     def rearrangeCharacters(self, s: str, target: str) -> int:
-        s = Counter(s)
-        target = Counter(target)
-        counter = 0
-        while True:
-            for char, count in target.items():
-                if char not in s or s[char] < count:
-                    return counter
-                s[char] -= count
-            counter += 1
-        return counter
+        # s = Counter(s)
+        # target = Counter(target)
+        # counter = 0
+        # while True:
+        #     for char, count in target.items():
+        #         if char not in s or s[char] < count:
+        #             return counter
+        #         s[char] -= count
+        #     counter += 1
+        # return counter
+        s_count = Counter(s)
+        t_count = Counter(target)
+
+        # For each char in target, compute how many times it can be formed
+        # using s, then return the minimum of those.
+        return min(s_count[c] // t_count[c] for c in t_count)
