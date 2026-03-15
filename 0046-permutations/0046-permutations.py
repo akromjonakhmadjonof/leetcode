524d1704cbf0e35e6defc1bd648779268ebf6580
Akromjon Akhmadjonov <akromjonakhmadjonoff@gmail.com>
2026-03-15 10:45:22 -0400

    Time: 4 ms (10.41%), Space: 19.4 MB (94.51%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,22 +1,13 @@
 class Solution:
-    def backtracking(self, nums, used, current, results):
-        if len(current) == len(nums):
-            results.append(current[:])
-            return
-        for index, num in enumerate(nums):
-            if used[index] == True:
-                continue
-            used[index] = True
-            current.append(num)
-            self.backtracking(nums, used, current, results)
-            current.pop()
-            used[index] = False
     def permute(self, nums: List[int]) -> List[List[int]]:
-        results = []
-        n = len(nums)
-        used = [False] * n
-        current = []
-             
+        if not nums:
+            return [[]]
 
-        self.backtracking(nums, used, current, results)
+        perms = self.permute(nums[1:])
+        results = []
+        for p in perms:
+            for i in range(len(p) + 1):
+                p_copy = p.copy()
+                p_copy.insert(i, nums[0])
+                results.append(p_copy)
         return results
